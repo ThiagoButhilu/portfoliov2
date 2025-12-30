cca7955f40380ca41bc6d55b1bfd051609a20c82
THICO <thiago@imaxgames.com.br>
2025-12-30 14:38:13 -0300

    add motion

--- src/components/Project/project.js
+++ src/components/Project/project.js
@@ -50,7 +50,7 @@ const Project = () => {
             initial={{ opacity: 0, y: 30 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: index * 0.1 }}
-            viewport={{ once: true }}
+            viewport={{ once: false }}
             whileHover={{ scale: 1.03 }}
           >
            
